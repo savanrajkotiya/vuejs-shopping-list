9c0054077f0eb329e52ad764a5f4098527164176
Savan Rajkotiya <savanrajkotiya@gmail.com>
2020-04-02 19:35:53 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ShoppingList />
+  </div>
+</template>
+
+<script>
+import ShoppingList from "./components/ShoppingList.vue";
+
+export default {
+  name: "app",
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    ShoppingList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/shared/data/products.js
@@ -0,0 +1,17 @@
+export const productsList = [
+    {
+      id: 1,
+      name: "10 party hats",
+      isStock: true
+    },
+    {
+      id: 2,
+      name: "cricket ball",
+      isStock: false
+    },
+    {
+      id: 3,
+      name: "2 board games",
+      isStock: true
+    }
+  ]
